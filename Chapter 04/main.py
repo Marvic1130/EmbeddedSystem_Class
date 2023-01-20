8f1dc1504055c867c3359e1125a38e25e5b12766
Marvic1130 <marvic1130@gmail.com>
2023-01-20 19:28:35 +0900

    DHT update

--- Chapter 04/main.py
+++ Chapter 04/main.py
@@ -2,11 +2,19 @@
 import adafruit_dht
 import time
 
-dht_device = adafruit_dht.DHT11(4) # 4는 pin번호
-
+dht_device = adafruit_dht.DHT22(4) # 4는 pin번호
 
 while(True):
-    if time.time()%5 == 0: # 5초에 한번
-        temperature = dht_device.temperature # 온도값 가져오기
-        humidity = dht_device.humidity # 습도값 가져오기
-        print("Temp: " + str(temperature) + "C, Humidity: " + str(humidity) + "%")
+    try:
+            temperature = dht_device.temperature # 온도값 가져오기
+            humidity = dht_device.humidity # 습도값 가져오기
+            print("Temp: " + str(temperature) + "C, Humidity: " + str(humidity) + "%")
+            time.sleep(3)
+
+    except RuntimeError as error:
+
+        print(error.args[0])
+
+    except Exception as error:
+        dht_device.exit()
+        raise error
